48229b243a3c19caefaa7e1b236d345768a5d143
BigBey <bey.sviatoslav@yandex.ru>
2019-08-26 09:31:53 +0300

    add Registration Form Activity

---
+++ app/src/main/java/com/example/school_programm_as/RegistrationFormActivity.java
@@ -0,0 +1,22 @@
+package com.example.school_programm_as;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class RegistrationFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form);
+        
+    }
+
+}
