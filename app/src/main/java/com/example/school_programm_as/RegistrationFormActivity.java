03521c239746f2d3630a5671fd009cdf495a0232
BigBey <bey.sviatoslav@yandex.ru>
2019-08-27 11:00:36 +0300

    add FireBase

--- app/src/main/java/com/example/school_programm_as/RegistrationFormActivity.java
+++ app/src/main/java/com/example/school_programm_as/RegistrationFormActivity.java
@@ -4,22 +4,37 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegistrationFormActivity extends AppCompatActivity {
+public class RegistrationFormActivity extends AppCompatActivity{
 
+    private FirebaseAuth mAuth;
+    private EditText ETEmail;
+    private EditText ETPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
+
+        mAuth = FirebaseAuth.getInstance();
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(RegisterActivity.EXTRA_MESSAGE);
         ImageView imageView =  findViewById(R.id.imageView3);
@@ -41,11 +56,39 @@ public class RegistrationFormActivity extends AppCompatActivity {
             imageView.setImageResource(id);
         }
 
+        ETEmail = (EditText)findViewById(R.id.editText4);
+        ETPassword = (EditText)findViewById(R.id.editText5);
+
     }
 
+
+    public void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(RegistrationFormActivity.this, "Регистрация прошла успешно",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(RegistrationFormActivity.this, "Регистрация провалена",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
     public void Back(View view){
         Intent intentBack = new Intent(this, RegisterActivity.class);
         startActivity(intentBack);
     }
 
+    public void signUp(View view) {
+            createAccount(ETEmail.getText().toString(), ETPassword.getText().toString());
+    }
 }
