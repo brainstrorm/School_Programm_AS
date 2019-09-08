59b4d75700473275dbfc90d23b850e492c876c7e
BigBey <bey.sviatoslav@yandex.ru>
2019-09-08 15:06:00 +0300

    add Camera View to QR scanner

--- app/src/main/java/com/example/school_programm_as/QRScan.java
+++ app/src/main/java/com/example/school_programm_as/QRScan.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.Rect;
@@ -14,6 +15,8 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,28 +29,90 @@ import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.List;
 
+import github.nisrulz.qreader.QRDataListener;
+import github.nisrulz.qreader.QREader;
+
 public class QRScan extends AppCompatActivity {
 
-    SurfaceView surfaceView;
-    TextView txtBarcodeValue;
-    private BarcodeDetector barcodeDetector;
-    private CameraSource cameraSource;
-    private static final int REQUEST_CAMERA_PERMISSION = 201;
-    Button btnAction;
-    String intentData = "";
-    boolean isEmail = false;
+    private TextView txt_result;
+    private SurfaceView surfaceView;
+    private QREader qrEader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan);
 
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        setupCamera();
+                        
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(QRScan.this, "You must enable this permission", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
 
 
 
 
+
+    }
+
+    private void setupCamera() {
+        txt_result = (TextView) findViewById(R.id.textView5);
+        final ToggleButton btn_on_off = (ToggleButton) findViewById(R.id.btn_enable_disable);
+        btn_on_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(qrEader.isCameraRunning()){
+                    btn_on_off.setChecked(false);
+                    qrEader.stop();
+                }else{
+                    btn_on_off.setChecked(true);
+                    qrEader.start();
+                }
+            }
+        });
+        surfaceView = (SurfaceView) findViewById(R.id.camera_view);
+        setupQREader();
+    }
+
+    private void setupQREader() {
+        qrEader = new QREader.Builder(this, surfaceView, new QRDataListener() {
+            @Override
+            public void onDetected(final String data){
+                txt_result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        txt_result.setText(data);
+                    }
+                });
+            }
+        }).facing(QREader.BACK_CAM)
+                .enableAutofocus(true)
+                .height(surfaceView.getHeight())
+                .width(surfaceView.getWidth())
+                .build();
     }
 
     public void Back(View view){
@@ -58,5 +123,59 @@ public class QRScan extends AppCompatActivity {
     public void Enter(View view){
         Intent intentEnter = new Intent(this, MyQR.class);
         startActivity(intentEnter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        if(qrEader != null){
+                            qrEader.initAndStart(surfaceView);
+                        }
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(QRScan.this, "You must enable this permission", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        if(qrEader != null){
+                            qrEader.releaseAndCleanup();
+                        }
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(QRScan.this, "You must enable this permission", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
     }
 }
