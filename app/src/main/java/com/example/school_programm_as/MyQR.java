706a2161827070c13003c21a0317483b513e92eb
BigBey <bey.sviatoslav@yandex.ru>
2019-09-13 01:06:17 +0300

    add QRCode creator to MyQR activity

--- app/src/main/java/com/example/school_programm_as/MyQR.java
+++ app/src/main/java/com/example/school_programm_as/MyQR.java
@@ -3,13 +3,33 @@ package com.example.school_programm_as;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class MyQR extends AppCompatActivity {
 
     String userId,groupId;
 
+    private ImageView IVQRCode;
+    private FirebaseFirestore mFirestore;
+    private String TAG = "GenerateQRCode";
+    private Bitmap bitmap;
+    private QRGEncoder qrgEncoder;
+    private String savePath = Environment.getExternalStorageDirectory().getPath() + "/QRCode/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +43,43 @@ public class MyQR extends AppCompatActivity {
             groupId = extras.getString("GROUP_ID_MESSAGE");
         }
 
+        IVQRCode = (ImageView) findViewById(R.id.imageView11);
+
+        if(groupId.length() > 0){
+            WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+            Display display = manager.getDefaultDisplay();
+            Point point = new Point();
+            display.getSize(point);
+            int width = point.x;
+            int height = point.y;
+            int smallerDimension = width < height ? width : height;
+            smallerDimension = smallerDimension * 3 / 4;
+
+            qrgEncoder = new QRGEncoder(
+                    userId, null,
+                    QRGContents.Type.TEXT,
+                    smallerDimension);
+            try {
+                bitmap = qrgEncoder.encodeAsBitmap();
+                IVQRCode.setImageBitmap(bitmap);
+            } catch (WriterException e) {
+                Log.v(TAG, e.toString());
+            }
+        }else {
+            Toast.makeText(getApplicationContext(), "Required", Toast.LENGTH_SHORT).show();
+        }
+
+        /*boolean save;
+        String result;
+        try {
+            save = QRGSaver.save(savePath, groupId, bitmap, QRGContents.ImageType.IMAGE_JPEG);
+            result = save ? "Image Saved" : "Image Not Saved";
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
+    
 
     public void Back(View view){
         Intent intentMyQRActivity = new Intent(getApplicationContext(), QRScan.class);
